f36c5328262019ac81b44babf74c2e26b5450a85
Majingjing <1246944828@qq.com>
2016-03-04 09:33:02 +0800

    Merge branch 'master' of https://github.com/Majingjing/Car
    Conflicts:
    	Car 3 2/Car/Class/Resourses/8879-11091423003686.jpg

---
+++ Car 3 2/Car/Class/Models/ChaiCheModel.h
@@ -0,0 +1,20 @@
+//
+//  ChaiCheModel.h
+//  Car
+//
+//  Created by lanou3g on 16/3/3.
+//  Copyright © 2016年 麻静. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ChaiCheModel : NSObject
+
+
+
+@property (nonatomic,strong) NSString *title;
+@property (nonatomic,strong) NSString *url;
+@property (nonatomic,strong) NSString *covimg;
+
+
+@end
